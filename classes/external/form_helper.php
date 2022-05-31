3cb3bb6a0e18737fceb970374d23ef69a5a3f568
Octavio Requena <91325424+octaviorequena@users.noreply.github.com>
2022-05-31 15:37:48 +0200

    (Reload Users 1/3) Add javasscript file to mod_form to refresh user list with AJAX
    * add javasscript file to mod_form to refresh user list with AJAX
    
    * fix bug with only some user getting the credential and add tests
    
    * send users instead of context and render warning if no group selected
    
    * refactor users class and mod_form
    
    * rescue api error and add test case
    
    * move for loop outside try-catch

---
+++ classes/external/form_helper.php
@@ -0,0 +1,85 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_accredible\external;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->libdir  . '/externallib.php');
+require_once($CFG->dirroot . '/mod/accredible/locallib.php');
+
+use mod_accredible\local\users;
+
+/**
+ * Web service end point for the reload users filter.
+ *
+ * @package    mod_accredible
+ * @subpackage accredible
+ * @since      Moodle 27
+ * @copyright  Accredible <dev@accredible.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class form_helper extends \external_api {
+    /**
+     * Returns parameter types for reload_users function.
+     *
+     * @return \external_function_parameters Parameters
+     */
+    public static function reload_users_parameters() {
+        return new \external_function_parameters([
+            'courseid' => new \external_value(PARAM_INT, 'Course id.'),
+            'groupid' => new \external_value(PARAM_INT, 'Group id.'),
+        ]);
+    }
+
+    /**
+     * Returns response schema for reload_users function.
+     *
+     * @return \external_description Result type
+     */
+    public static function reload_users_returns() {
+        return new \external_multiple_structure(
+            new \external_single_structure([
+                    'id' => new \external_value(PARAM_RAW, 'User ID.'),
+                    'email' => new \external_value(PARAM_RAW, 'User email'),
+                    'name' => new \external_value(PARAM_RAW, 'User name'),
+                    'credential_url' => new \external_value(PARAM_RAW, 'Credential URL.'),
+                    'credential_id' => new \external_value(PARAM_RAW, 'Credential ID.'),
+            ]));
+    }
+
+    /**
+     * Get the list of users enrolled to a course and fetch their credential by groupid.
+     *
+     * @param int $courseid the course from moodle to load the enrolled users.
+     * @param int $groupid the group from accredible to check users certificates.
+     *
+     * @return array of users.
+     */
+    public static function reload_users($courseid, $groupid) {
+        $params = self::validate_parameters(self::reload_users_parameters(), array('courseid' => $courseid, 'groupid' => $groupid));
+        $context = \context_course::instance($courseid);
+        self::validate_context($context);
+
+        $enrolledusers = get_enrolled_users($context, "mod/accredible:view", null, 'u.*', 'id');
+        $userhelper = new users();
+        $users = $userhelper->get_users_with_credentials($enrolledusers, $groupid);
+
+        return $users;
+    }
+}
---
+++ classes/local/users.php
@@ -0,0 +1,109 @@
+<?php
+// This file is part of the Accredible Certificate module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_accredible\local;
+
+use mod_accredible\apirest\apirest;
+use mod_accredible\local\credentials;
+
+/**
+ * Local functions related to credentials.
+ *
+ * @package    mod_accredible
+ * @subpackage accredible
+ * @copyright  Accredible <dev@accredible.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class users {
+    /**
+     * HTTP request apirest.
+     * @var apirest
+     */
+    private $apirest;
+
+    /**
+     * Constructor method
+     *
+     * @param stdObject $apirest a mock apirest for testing.
+     */
+    public function __construct($apirest = null) {
+        // A mock apirest is passed when unit testing.
+        if ($apirest) {
+            $this->apirest = $apirest;
+        } else {
+            $this->apirest = new apirest();
+        }
+    }
+
+    /**
+     * Receive a list of users and fetch their credentials from
+     * the accredible group provided.
+     *
+     * @param array $enrolledusers array of users
+     * @param int $groupid accredible group id
+     * @return array the list of users
+     */
+    public function get_users_with_credentials($enrolledusers, $groupid = null) {
+        $users = array();
+        $certificates = array();
+
+        if (!$enrolledusers) {
+            return $users;
+        }
+
+        $certificatesmemo = array();
+        if ($groupid) {
+            try {
+                $credentialsclient = new credentials($this->apirest);
+                $certificates = $credentialsclient->get_credentials($groupid);
+            } catch (\moodle_exception $e) {
+                return $users;
+            }
+
+            foreach ($certificates as $certificate) {
+                if (isset($certificate->url)) {
+                    $credentialurl = $certificate->url;
+                } else {
+                    $credentialurl = 'https://www.credential.net/' . $certificate->id;
+                }
+                $certificatesmemo[$certificate->recipient->email] = array(
+                    'credentialid' => $certificate->id,
+                    'credentialurl' => $credentialurl
+                );
+            }
+        }
+
+        foreach ($enrolledusers as $user) {
+            if (isset($certificatesmemo[strtolower($user->email)])) {
+                $certificate = $certificatesmemo[strtolower($user->email)];
+            } else {
+                $certificate = null;
+            }
+
+            $credentialurl = isset($certificate) ? $certificate['credentialurl'] : null;
+            $credentialid = isset($certificate) ? $certificate['credentialid'] : null;
+            $user = array(
+                'id'             => $user->id,
+                'email'          => $user->email,
+                'name'           => $user->firstname . ' ' . $user->lastname,
+                'credential_url' => $credentialurl,
+                'credential_id'  => $credentialid
+            );
+            array_push($users, $user);
+        }
+        return $users;
+    }
+}
