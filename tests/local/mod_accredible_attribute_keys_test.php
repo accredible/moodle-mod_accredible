17516c59931120d53c07a64bd25a3d2c75587128
Martin Ndibui <muhingan@gmail.com>
2025-11-20 12:30:51 +0300

    remove unnecessary error assignment in mock

--- tests/local/mod_accredible_attribute_keys_test.php
+++ tests/local/mod_accredible_attribute_keys_test.php
@@ -103,7 +103,6 @@ class mod_accredible_attribute_keys_test extends \advanced_testcase {
             ->getMock();
 
         // Mock API response data.
-        $mockclient2->error = 'The requested URL returned error: 401 Unauthorized';
         $resdata = $this->mockapi->resdata('unauthorized_error.json');
 
         // Expect to call the endpoint once with page and page_size.
